b4edd5096efbd7952acb86dff62180a1c0b39adf
rezaze <reza8zy1@gmail.com>
2022-04-10 15:34:01 +0430

    Feedback

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelectorAll(".rate");
+const submit = document.getElementById("submit")
+const href = document.querySelector("a");
+const getAnimation = document.querySelector(".rates")
+
+function showNum(e) {
+
+    for (let i = 0; i <= btn.length; i++) {
+        btn[i].addEventListener("click", () => {
+            let currect = document.querySelector(".active");
+            btn[i].classList.add("active");
+            currect.className = currect.className.replace("active", "");
+            btn[i].className += "active";
+            href.setAttribute("href", "result.html");
+        })
+
+
+    }
+
+}
+
+showNum();
