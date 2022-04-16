b71802f38c2e44359fd5ea165eca6050660098f6
rezaze <reza8zy1@gmail.com>
2022-04-17 03:22:42 +0430

    Update Feedback

--- js/main.js
+++ js/main.js
@@ -16,47 +16,69 @@ function showNum() {
 
 
 
-const setCookie = (name, value, exday) => {
-    let d = new Date();
-    d.setTime(d.getTime() + (exday * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toGMTString();
-    document.cookie = name + " = " + value + ";" + expires + ";path=/";
+// const setCookie = (name, value, exday) => {
+//     let d = new Date();
+//     d.setTime(d.getTime() + (exday * 24 * 60 * 60 * 1000));
+//     let expires = "expires=" + d.toGMTString();
+//     document.cookie = name + " = " + value + ";" + expires + ";path=/";
 
-}
-const getCookie = (name) => {
-    var name = name + "=";
-    let decodeCookie = decodeURIComponent(document.cookie);
-    let cook = decodeCookie.split(';');
-    for (let i = 0; i < cook.length; i++) {
+// }
+// const getCookie = (name) => {
+//     var name = name + "=";
+//     let decodeCookie = decodeURIComponent(document.cookie);
+//     let cook = decodeCookie.split(';');
+//     for (let i = 0; i < cook.length; i++) {
 
-        var c = cook[i];
+//         var c = cook[i];
 
-        while (c.charAt(0) == ' ') {
+//         while (c.charAt(0) == ' ') {
 
-            c = c.substring(1);
-        }
+//             c = c.substring(1);
+//         }
 
-        if (c.indexOf(name == 0)) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+//         if (c.indexOf(name == 0)) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
 
-}
-const checkedcookie = () => {
-    const currect = document.querySelector(".active");
-    var user = getCookie("username");
-    user = currect.innerHTML;
-    if (user != "") {
-        setCookie('username', user, 3);
+// }
+// const checkedcookie = () => {
+//     const currect = document.querySelector(".active");
+//     var user = getCookie("username");
+//     user = currect.innerHTML;
+//     if (user != "") {
+//         setCookie('username', user, 3);
+//         window.open("result.html");
+//     }
+// }
+
+// function checkCookie() {
+//     let user = getCookie('username');
+//     showResult.innerHTML = `You selected ${user} out of 5`;
+//     setCookie("username", user, 3);
+// }
+
+
+
+
+////////////////// cookie
+
+
+function createItem() {
+    let currect = document.querySelector(".active");
+    sessionStorage.setItem("show", `You selected ${currect.innerHTML} out of 5`);
+    if (currect.innerHTML != "") {
         window.open("result.html");
     }
 }
 
-function checkCookie() {
-    let user = getCookie('username');
-    showResult.innerHTML = `You selected ${user} out of 5`;
-    setCookie("username", user, 3);
+function showItem() {
+    var x = sessionStorage["show"];
+    showResult.innerHTML = x;
 }
 
 showNum();
+
+
+////////////////storage(setItam);
